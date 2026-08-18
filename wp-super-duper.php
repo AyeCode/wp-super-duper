fa15ca4fa739da53f12c7f7165f1d719d9a3ddf5
Kiran Prajapati <officeofkiran@gmail.com>
2026-08-18 12:06:59 +0530

    1.2.35

--- wp-super-duper.php
+++ wp-super-duper.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'WP_Super_Duper' ) ) {
 
-	define( 'SUPER_DUPER_VER', '1.2.34' );
+	define( 'SUPER_DUPER_VER', '1.2.35' );
 
 	/**
 	 * A Class to be able to create a Widget, Shortcode or Block to be able to output content for WordPress.
